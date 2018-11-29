3a601abab1c2d6b4ba88a7822f53fc7f3ec7c4c9
EnzoRobaina <enzo.robaina@gmail.com>
2018-11-29 16:00:27 -0200

    Altera o campo status no modelo de usuário

--- androidapi/api/models.py
+++ androidapi/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class Pessoa(models.Model):
     nascimento = models.DateField(null=True, blank=True)
-    status = models.BooleanField(null=False, blank=False, default=True)
+    status = models.IntegerField(null=False, blank=False, default=0)
     endereco = models.CharField(max_length=100, blank=True)
     telefone = models.CharField(max_length=15, blank=True)
 
@@ -29,9 +29,10 @@ class UsuarioManager(BaseUserManager):
         person = self.create_user(
             username,
             email,
-            password
+            password,
         )
 
+        person.status = 1
         person.is_superuser = True
         person.save(using=self._db)
         return person
